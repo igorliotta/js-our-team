fafc023b5c0ec874c3c961ab84a456f97e23df1d
igorliotta <liottaigor@gmail.com>
2023-10-18 17:44:43 +0200

    Added: grid and his content with js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,7 @@
 console.log('Our Team');
 
-// const gridElement = document.querySelector('.grid');
-// console.log(gridElement);
-
-// const memberBoxElement = document.querySelector('.member-box');
-// console.log(memberBoxElement);
-
-fullnameElement = document.getElementById('fullname');
-console.log(fullnameElement);
-
-professionElement = document.getElementById('profession');
-console.log(professionElement);
-
-imgElement = document.getElementById('img');
-console.log(imgElement);
+const gridElement = document.querySelector('.grid');
+console.log(gridElement);
 
 const members = [
     {
@@ -24,27 +12,27 @@ const members = [
     {
         fullname: 'Angela Caroll',
         profession: 'Chief Editor',
-        memberImgString: 'angela-caroll-chief-editor.jpg',
+        memberImg: 'angela-caroll-chief-editor.jpg',
     },
     {
         fullname: 'Walter Gordon',
         profession: 'Office Manager',
-        memberImgString: 'walter-gordon-office-manager.jpg',
+        memberImg: 'walter-gordon-office-manager.jpg',
     },
     {
         fullname: 'Angela Lopez',
         profession: 'Social Media Manager',
-        memberImgString: 'angela-lopez-social-media-manager.jpg',
+        memberImg: 'angela-lopez-social-media-manager.jpg',
     },
     {
         fullname: 'Scott Estrada',
         profession: 'Developer',
-        memberImgString: 'scott-estrada-developer.jpg',
+        memberImg: 'scott-estrada-developer.jpg',
     },
     {
         fullname: 'Barbara Ramos',
         profession: 'Graphic Designer',
-        memberImgString: 'barbara-ramos-graphic-designer.jpg',
+        memberImg: 'barbara-ramos-graphic-designer.jpg',
     }
 ];
 
@@ -55,25 +43,22 @@ for (let i = 0; i < members.length; i++) {
      const currentMember = members[i];
      console.log(currentMember, i);
 
-     const currentMemberFullnameString = JSON.stringify(currentMember.fullname);
-     console.log(currentMemberFullnameString);
-
-     const currentMemberProfessionString = JSON.stringify(currentMember.profession);
-     console.log(currentMemberProfessionString);
-
-     const currentMemberMemberImgString = JSON.stringify(currentMember.memberImgString);
-     console.log(currentMemberMemberImgString);
-
-     const currentMemberFullString = currentMemberFullnameString + " " + currentMemberProfessionString + " " + currentMemberMemberImgString;
-     console.log(currentMemberFullString);
-
+     for (let i = 0; i < 1; i++) {
+        n = i + 1;
+        console.log(n);
     
-    //  for (let i = 0; i < gridElement.length; i++) {
-    //     fullnameElement.innerHTML = currentMemberFullnameString;
-    //     professionElement.innerHTML = currentMemberProfessionString;
-    //     imgElement.innerHTML = currentMemberMemberImgString;
-   
-    //  }
+        const htmlBox = 
+        ` <div class="member-box">
+            <span id="fullname">${currentMember.fullname}</span>
+            <span id="profession">${currentMember.profession}</span>
+            <img src"" alt"">${currentMember.memberImg}</img>
+         </div>`;
+
+        
+    
+        gridElement.innerHTML = gridElement.innerHTML + htmlBox;
+    }
+//  console.log(imgElement);
 }
 
 

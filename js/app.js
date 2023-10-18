21999e11a627610ca9dfcadef0cb9faaa27fbfee
igorliotta <liottaigor@gmail.com>
2023-10-18 15:31:37 +0200

    Added: members array in js

---
+++ js/app.js
@@ -0,0 +1,36 @@
+console.log('Our Team');
+
+const members = [
+    {
+        fullname: 'Wayne Barnett',
+        profession: 'Founder & CEO',
+        memberImg: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        fullname: 'Angela Caroll',
+        profession: 'Chief Editor',
+        memberImg: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        fullname: 'Walter Gordon',
+        profession: 'Office Manager',
+        memberImg: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        fullname: 'Angela Lopez',
+        profession: 'Social Media Manager',
+        memberImg: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        fullname: 'Scott Estrada',
+        profession: 'Developer',
+        memberImg: 'scott-estrada-developer.jpg',
+    },
+    {
+        fullname: 'Barbara Ramos',
+        profession: 'Graphic Designer',
+        memberImg: 'barbara-ramos-graphic-designer.jpg',
+    }
+];
+
+console.log(members);
